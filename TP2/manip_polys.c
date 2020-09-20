19cadab47c287337a9d02fe9957d4f6c4d373dd6
Melet Chirino <melet.chirino@gmail.com>
2020-09-21 00:45:27 +0200

    Added files for TP2

---
+++ TP2/manip_polys.c
@@ -0,0 +1,11 @@
+//Par CHIRINO et NAVARRO le 12/sep/2020
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	/* Loi des moindres carres
+	 * y a quelque chose qui va pas
+	 * */
+
+	return 0;
+}
