cd9319d22a71e6489a7db6f5b1f938b37d381799
MeletChirino <melet.chirino@gmail.com>
2020-10-11 02:32:44 +0200

    TP2 listo con funciones que multiplican y derivan polinomios, como agregado podrias ponerle una funcion que haga polinomios con base a algun vector de elemento que funcione como las raices del polinomio. Con eso puedes hacer cosillas interesantes para el proximo TP de interpolacion

--- TP2/fonctions.h
+++ TP2/fonctions.h
@@ -2,15 +2,80 @@ struct polynomes{
 		int degree;
 		float *coeff;
 };
+struct polynomes produit(struct polynomes poly1, struct polynomes poly2){
+	int i, j;
+	float coeff1, coeff2;
+	struct polynomes poly_produit;
+	poly_produit.degree = poly1.degree + poly2.degree;
+	poly_produit.coeff = (float*)malloc(poly_produit.degree*sizeof(float));
+	for(i=0; i<= poly_produit.degree; i++)
+		poly_produit.coeff[i] = 0;
+
+	for(i=0; i<= poly1.degree; i++){
+		if(poly1.coeff[i]){
+			coeff1 = poly1.coeff[i];
+			for(j=0; j<= poly2.degree; j++){
+				if(poly2.coeff[j]){
+					coeff2 = poly2.coeff[j];
+					poly_produit.coeff[i+j] += coeff1 * coeff2;
+				printf("\n%0.1fx^%d * %0.1fx^%d = %0.1fx^%d",coeff1, i, coeff2, j, poly_produit.coeff[i+j], i+j);
+				}
+			}
+
+		}
+	} 
+	return poly_produit;
+}
+
 void coeff_poly(struct polynomes *poly){
 	int i;
 	for(i=0; i < poly->degree + 1; i++){
 		printf("\ncoeff[%d] = ",i);
-		scanf("%f", &poly.coeff[i]);
+		scanf("%f", &poly->coeff[i]);
 	}
 
 }
+float higher(float num1, float num2){
+	if(num1 >= num2)
+		return num1;
+	else if(num2 > num1)
+		return num2;
+}
 
+struct polynomes somme(struct polynomes poly1, struct polynomes poly2){
+	int i;
+	float coeff1, coeff2;
+	struct polynomes poly_somme;
+	poly_somme.degree = higher(poly1.degree, poly2.degree);
+	poly_somme.coeff = (float*)malloc(poly_somme.degree*sizeof(float));
+
+	for(i = 0; i < poly_somme.degree+1; i++){
+		if(!poly1.coeff[i])
+			coeff1 = 0;
+		else
+			coeff1 = poly1.coeff[i];
+		if(!poly2.coeff[i])
+			coeff2 = 0;
+		else
+			coeff2 = poly2.coeff[i];
+		poly_somme.coeff[i] = coeff1 + coeff2;
+	}
+
+	return poly_somme;
+}
+
+struct polynomes deriver(struct polynomes poly){
+	int taille = poly.degree, i;
+	struct polynomes poly_derive;
+	poly_derive.degree = taille - 1;
+	poly_derive.coeff = (float*)malloc(poly_derive.degree*sizeof(float));
+	for(i = 1; i < taille+1; i++){
+		poly_derive.coeff[i-1] = i * poly.coeff[i];
+		printf("\n%d * %f = %f", i, poly.coeff[i], poly_derive.coeff[i-1]);
+	}
+	return poly_derive;
+
+}
 struct polynomes create_poly(){
 	int taille;
 	struct polynomes poly;
--- TP2/manip_polys.c
+++ TP2/manip_polys.c
@@ -8,28 +8,37 @@ int main(){
 	/* Loi des moindres carres
 	 * y a quelque chose qui va pas
 	 * */
-	int taille, i;
-	float valeur;
 	struct polynomes poly1 = create_poly();
-
-	//for(i =0; i< poly1.degree+1; i++){
-//		printf("\ncoeff[%d] ",i);
-//		scanf("%f", &poly1.coeff[i]);
-//	}
 	coeff_poly(&poly1);
 
 		printf("\n\n\n");
 
 	affichage_polynome(poly1);
 
-	do{
-		printf("\nSaisisez un valeur a calculer avec le polynome trouve\n");
-		scanf("%f",&valeur);
-		printf("\ny(%0.2f) = %f",valeur, poly_evalue(poly1, valeur));
-		
-	}while(valeur != -1);
+	//teste derivee
+	struct polynomes poly2 = deriver(poly1);
+	printf("\nDerivee = ");
+	affichage_polynome(poly2);
+	printf("\n\n");
 
+	// teste somme avec la derivee du polynome
+	struct polynomes poly3 = somme(poly1, poly2);
+	printf("\nSomme = ");
+	affichage_polynome(poly3);
+	printf("\n\n");
 
+	// teste multiplication avec la derivee du polynome
+	struct polynomes poly4 = produit(poly1, poly2);
+	printf("\nMultiplication = ");
+	affichage_polynome(poly4);
+	printf("\n\n");
+
+	//teste multiplication avec un nouvelle polynome
+	struct polynomes poly5 = create_poly();
+	coeff_poly(&poly5);
+	struct polynomes poly6 = produit(poly1, poly5);
+	printf("\nMultiplication = ");
+	affichage_polynome(poly6);
 	printf("\n\n");
 
 	return 0;
