54904bbc54f6fa23e77bd23ed98b10cc862d784d
navarro gregoire <nvg2585a@ens.fsi>
2020-10-19 11:37:52 +0200

    fichiers de quadratures ajoutees

--- integration/main.c
+++ integration/main.c
@@ -1,7 +1,22 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include "TP5_library.h"
 
-void main(){
-
+int main(){
+    
+		structQuadrature pQuadrature;
+              
+        //initialisation des tableaux avec la taille de n
+    
+        LireQuadrature(&pQuadrature); 
+        
+        printf("X = ");
+		show_array(pQuadrature.tabZeta, pQuadrature.taille);
+		printf("\n\n");
+		printf("Y = ");
+		show_array(pQuadrature.tabOmega, pQuadrature.taille);
+           
+        
+        system("PAUSE");
+		return 0;
 }
-
