854c9d9fd87feaade26f0c7e2b9fa80205a46ee4
Melet Chirino <melet.chirino@gmail.com>
2020-10-19 12:15:10 +0200

    Everything works in main.c plus a little change in m_dt.txt

--- integration/TP5_library.h
+++ integration/TP5_library.h
@@ -3,6 +3,7 @@
 /* Fichier de base pour le sujet de TP 5 */
 //
 // Les structures suivantes sont imposées
+#include <stdio.h>
 struct structIntervalle{
 	double debut;
 	double fin;
@@ -25,18 +26,40 @@ struct structEchantillonage_fonction{
 	double** matriceFonctionEchantillonnee;
 };
 
-void LireQuadrature(struct structQuadrature *pQuadrature){
+void LireQuadrature(struct structQuadrature* pQuadrature){
+	/* fonction pour lire une fichier avec des donnees */
+	FILE *file;
+	int x_, y_;
+	int i, n;
+	
+	file = fopen("m_pm.txt","r");
+	fscanf(file,"%d", &n);
+	
+	pQuadrature->taille = n;
+	
+	pQuadrature->tabZeta = (double*)malloc(pQuadrature->taille*sizeof(double));
+    pQuadrature->tabOmega = (double*)malloc(pQuadrature->taille*sizeof(double));  
 
+	//while(fscanf(file,"%lf %lf", &x_, &y_) != EOF)
+    for(i=0; i<n; i++)
+    {
+        fscanf(file,"%d %d", &x_, &y_);    
+        printf("%d %d", x_, y_);
+		pQuadrature->tabZeta[i] = (double)x_;
+		pQuadrature->tabOmega[i] = (double)y_;
+	}
+
+	fclose(file);
 }
 
 void CalculerSubdivisionReguliere(int n,struct structIntervalle Intervalle, struct structSubdivision* pSubdivision){
 	int i;
-	double h = (Intervalle.fin - Intervalle.debut) / (n+1);
+	double h = (Intervalle.fin - Intervalle.debut) / (n);
 	pSubdivision->nombre = n;
 	pSubdivision->tabIntervalle = (struct structIntervalle*)malloc(n*sizeof(struct structIntervalle));
 	for(i = 0; i < n; i++){
-		pSubdivision->tabIntervalle[i].debut = Intervalle.debut + (i*h);
-		pSubdivision->tabIntervalle[i].fin = pSubdivision->tabIntervalle[i].debut + (h*i);
+		pSubdivision->tabIntervalle[i].debut = Intervalle.debut + (double)(i*h);
+		pSubdivision->tabIntervalle[i].fin = pSubdivision->tabIntervalle[i].debut + (double)(h);
 	}
 
 }
@@ -54,7 +77,7 @@ double CalculIntegrale(structEchantillonnage_fonction Echantillonnage_fonction);
 
 /* FONCTIONS DEJA CODEES DANS LA LIBRAIRIE */
 void show_array(double *array, int n);
-void LireQuadrature(structQuadrature* pQuadrature);
+void LireQuadrature(struct structQuadrature* pQuadrature);
 double CalculFonction(double x);
 //void AffichageGnuplot(structEchantillonnage_fonction Echantillonnage_fonction);
 void SauvegardeCourbe(char * nom_fichier, double * Abscisses, double * Ordonnees, int n);
