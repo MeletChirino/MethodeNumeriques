6570ac4b9ecd613bffc53e4937484a644ff1acad
Melet Chirino <melet.chirino@gmail.com>
2020-10-19 11:17:40 +0200

    Calculer subdivisions marche bien

--- integration/TP5_library.h
+++ integration/TP5_library.h
@@ -3,28 +3,43 @@
 /* Fichier de base pour le sujet de TP 5 */
 
 // Les structures suivantes sont imposées
-typedef struct{
+struct structIntervalle{
 	double debut;
 	double fin;
-} structIntervalle;
+};
 
-typedef struct{
+struct structSubdivision{
 	int nombre;
-	structIntervalle* tabIntervalle;
-} structSubdivision;
+	struct structIntervalle* tabIntervalle;
+};
 
-typedef struct{
+struct structQuadrature{
 	int taille;
 	double* tabZeta;
 	double* tabOmega;
-} structQuadrature;
+};
 
-typedef struct{
-	structQuadrature Quadrature_Echantillonnage;
-	structSubdivision Subdivision_Echantillonnage;
+struct structEchantillonage_fonction{
+	struct structQuadrature Quadrature_Echantillonnage;
+	struct structSubdivision Subdivision_Echantillonnage;
 	double** matriceFonctionEchantillonnee;
-} structEchantillonnage_fonction;
+};
 
+void LireQuadrature(struct structQuadrature *pQuadrature){
+
+}
+
+void CalculerSubdivisionReguliere(int n,struct structIntervalle Intervalle, struct structSubdivision* pSubdivision){
+	int i;
+	double h = (Intervalle.fin - Intervalle.debut) / (n+1);
+	pSubdivision->nombre = n;
+	pSubdivision->tabIntervalle = (struct structIntervalle*)malloc(n*sizeof(struct structIntervalle));
+	for(i = 0; i < n; i++){
+		pSubdivision->tabIntervalle[i].debut = Intervalle.debut + (i*h);
+		pSubdivision->tabIntervalle[i].fin = pSubdivision->tabIntervalle[i].debut + (h*i);
+	}
+
+}
 
 /* PROPOSITIONS DE FONCTIONS :
 void CalculerSubdivisionReguliere(int n, structIntervalle Intervalle, structSubdivision* pSubdivision);
@@ -36,7 +51,7 @@ double CalculIntegrale(structEchantillonnage_fonction Echantillonnage_fonction);
 
 /* FONCTIONS DEJA CODEES DANS LA LIBRAIRIE */
 double CalculFonction(double x);
-void AffichageGnuplot(structEchantillonnage_fonction Echantillonnage_fonction);
+//void AffichageGnuplot(structEchantillonnage_fonction Echantillonnage_fonction);
 void SauvegardeCourbe(char * nom_fichier, double * Abscisses, double * Ordonnees, int n);
 
 #endif
