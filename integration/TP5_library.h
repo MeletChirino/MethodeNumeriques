79cc08239f59aaf5d2bedf8cae5f28c5e5d358de
MeletChirino <melet.chirino@gmail.com>
2020-10-26 17:36:30 +0100

    The program requests the user some info for Integral Calcule and works fine with fonction provided by the teacher. Function AffichageGnuPlot is not working yet, there are some allocating issues that does not allow my computer to make it work by now.

--- integration/TP5_library.h
+++ integration/TP5_library.h
@@ -4,47 +4,106 @@
 //
 // Les structures suivantes sont imposées
 #include <stdio.h>
+#include <math.h>
+
 typedef struct{
-	double debut;
-	double fin;
+		double debut;
+		double fin;
 } structIntervalle;
 
 typedef struct{
-	int nombre;
-	structIntervalle* tabIntervalle;
+		int nombre;
+		structIntervalle* tabIntervalle;
 } structSubdivision;
 
 typedef struct{
-	int taille;
-	double* tabZeta;
-	double* tabOmega;
+			int taille;
+			double* tabZeta;
+			double* tabOmega;
 } structQuadrature;
 
 typedef struct{
-	structQuadrature Quadrature_Echantillonnage;
-	structSubdivision Subdivision_Echantillonnage;
-	double** matriceFonctionEchantillonnee;
+			structQuadrature Quadrature_Echantillonage;
+			structSubdivision Subdivision_Echantillonage;
+			double** matriceFonctionEchantillonnee;
 } structEchantillonage_fonction;
 
+double CalculFonction(double x) // On modifiera ici la fonction Ã  intÃ©grer
+{
+		double resultat;
+		resultat = exp(-x*x/2)/(sqrt(2*3.14159265358979));
+		return resultat;
+}
+
 double f(double x){
-		return x*x;
+		// f(x) = a;
+		//return 4;
+		//f(x) = ax + b;
+		//return 4*x + 4;
+		//f(x) = exp(-x);
+		return exp(-x);
+}
+
+double CalculIntegrale(structEchantillonage_fonction Echantillonage_fonction){
+		int i, j, n, taille_quad;
+		double I = 0, a_i, a_i1, sumQuadrature;
+		taille_quad = Echantillonage_fonction.Quadrature_Echantillonage.taille;
+
+		n = Echantillonage_fonction.Subdivision_Echantillonage.nombre;
+		printf("\nn = %d", n);
+
+		for(i=0; i < n-1; i++){
+			a_i = Echantillonage_fonction.Subdivision_Echantillonage.tabIntervalle[i].debut;
+			a_i1 = Echantillonage_fonction.Subdivision_Echantillonage.tabIntervalle[i].fin;
+
+			sumQuadrature = 0;
+			for(j=0; j<taille_quad; j++)
+				sumQuadrature += Echantillonage_fonction.matriceFonctionEchantillonnee[j][i];
+
+			//printf("\nI += (%lf - %lf) * %lf", a_i1, a_i, sumQuadrature);
+			I += (a_i1 - a_i) * (sumQuadrature);
+		printf("\nI = %lf", I);
+		}
+
+		return I;
+		//AffichageGnuplot(matriz);
 }
 
 structEchantillonage_fonction echantillonage_fonction(structSubdivision subdivision, structQuadrature quadrature){
 		//inicialicemos variables para esta funcion
-		int i, j = 0;
+		int i, j = 0, n;
 		double a_i, a_i1, w_j, zeta_i, term;
-
+		
+		//ahora creamos la matriz donde se van a almacenar nuestra matriz
 		structEchantillonage_fonction matrice_final;
-		matrice_final.matriceFonctionEchantillonnee = (double**)malloc(quadrature.taille*sizeof(double*));
-		for(i = 0; i < subdivision.nombre; i++)
-			matrice_final.matriceFonctionEchantillonnee[i] = (double*)malloc(subdivision.nombre*sizeof(double));
 
+		//Vamos a llenar la matriz de todos los valores predeterminados que debe tener
+		n = subdivision.nombre;
+		matrice_final.Subdivision_Echantillonage.nombre = n;
+		matrice_final.Subdivision_Echantillonage.tabIntervalle = (structIntervalle*)malloc(n*sizeof(structIntervalle));
+		for(i = 0; i < n; i++){
+			matrice_final.Subdivision_Echantillonage.tabIntervalle[i].debut = subdivision.tabIntervalle[i].debut;
+			matrice_final.Subdivision_Echantillonage.tabIntervalle[i].fin = subdivision.tabIntervalle[i].fin;
+		}
+		n = quadrature.taille;
+		matrice_final.Quadrature_Echantillonage.taille = n; 
+		matrice_final.Quadrature_Echantillonage.tabOmega = (double*)malloc(n*sizeof(double)); 
+		matrice_final.Quadrature_Echantillonage.tabZeta = (double*)malloc(n*sizeof(double)); 
+		for(i = 0; i < n; i++){
+			matrice_final.Quadrature_Echantillonage.tabZeta[i] = quadrature.tabZeta[i]; 
+			matrice_final.Quadrature_Echantillonage.tabOmega[i] = quadrature.tabOmega[i]; 
+		}
+		//finalmente podemos crear l matriz que vendra a ser nuestra matriz de sumatorias que usaremos en un futuro
+		matrice_final.matriceFonctionEchantillonnee = (double**)malloc(subdivision.nombre*sizeof(double*));
+		for(i = 0; i < quadrature.taille; i++)
+			matrice_final.matriceFonctionEchantillonnee[i] = (double*)malloc(quadrature.taille*sizeof(double));
+
+		//ahora se hacen los valores iniciales
 		for(i = 0; i < subdivision.nombre - 1; i++){
 
 			a_i = subdivision.tabIntervalle[i].debut;
 			a_i1 = subdivision.tabIntervalle[i].fin;
-			printf("\n");
+			//printf("\n");
 
 				for(j=0; j < quadrature.taille; j++){
 
@@ -53,36 +112,56 @@ structEchantillonage_fonction echantillonage_fonction(structSubdivision subdivis
 
 					term = ((1-zeta_i)*a_i + zeta_i*a_i1)*w_j;
 
-					matrice_final.matriceFonctionEchantillonnee[j][i] = f(term);
-					printf("%lf \t", f(term));
+					matrice_final.matriceFonctionEchantillonnee[j][i] = CalculFonction(term);
+					//porque es horizontal?
+					//printf("%lf \t", CalculFonction(term));
 				}
 		}
 		return matrice_final;
 }
 
-
-void LireQuadrature(structQuadrature* pQuadrature){
+void LireQuadrature(structQuadrature* pQuadrature, int type){
 		/* fonction pour lire une fichier avec des donnees */
 		FILE *file;
 		float x_, y_;
 		int i, n;
 		printf("\nreading Quadrature");
 
-		file = fopen("m_dt.txt","r");
+		switch(type){
+				case 1:
+					file = fopen("m_dt.txt","r");
+					break;
+				case 2:
+					file = fopen("m_pd.txt","r");
+					break;
+				case 3:
+					file = fopen("m_pg.txt","r");
+					break;
+				case 4:
+					file = fopen("m_pm.txt","r");
+					break;
+		}
+
+		if(file == NULL){
+				printf("\nfile does not exist\n");
+				exit(0);
+		}
 		fscanf(file,"%d", &n);
 
 		pQuadrature->taille = n;
-		printf ("\nQuadrature size = %d", n);
+		printf("\nQuadrature size = %d", pQuadrature->taille);
+		printf("\nInitializing tabs.");
 
-		pQuadrature->tabZeta = (double*)malloc(pQuadrature->taille*sizeof(double));
-		pQuadrature->tabOmega = (double*)malloc(pQuadrature->taille*sizeof(double));  
+		pQuadrature->tabZeta = (double*)malloc(n*sizeof(double));
+		printf(".");
+		pQuadrature->tabOmega = (double*)malloc(n*sizeof(double));  
+		printf(".\n");
 
-		//while(fscanf(file,"%lf %lf", &x_, &y_) != EOF)
 		for(i=0; i<n; i++){
 			fscanf(file,"%f %f", &x_, &y_);    
-			printf("\nline[%d] = %f %f", i, x_, y_);
-			pQuadrature->tabZeta[i] = (double)x_;
-			pQuadrature->tabOmega[i] = (double)y_;
+			pQuadrature->tabZeta[i] = x_;
+			pQuadrature->tabOmega[i] = y_;
+		//	printf("\nline[%d] = %f %f", i, x_, y_);
 		}
 
 		fclose(file);
@@ -97,22 +176,22 @@ void CalculerSubdivisionReguliere(int n,structIntervalle Intervalle,structSubdiv
 		pSubdivision->tabIntervalle[i].debut = Intervalle.debut + (double)(i*h);
 		pSubdivision->tabIntervalle[i].fin = pSubdivision->tabIntervalle[i].debut + (double)(h);
 		 }
+}
+void show_array(double *array, int n){
+		int i;
+		for(i = 0; i < n; i++)
+        {
+				printf("\n Ligne[%d] = %lf",i, array[i]);
+		}	
 
-/* PROPOSITIONS DE FONCTIONS :
-void CalculerSubdivisionReguliere(int n, structIntervalle Intervalle, structSubdivision* pSubdivision);
-
-
-
-void CalculerEchantillonnage(structQuadrature Quadrature, structSubdivision Subdivision,structEchantillonnage_fonction* pEchantillonnage_fonction);
-double CalculIntegrale(structEchantillonnage_fonction Echantillonnage_fonction);
-*/
-
-
-/* FONCTIONS DEJA CODEES DANS LA LIBRAIRIE */
-void show_array(double *array, int n);
-void LireQuadrature(struct structQuadrature* pQuadrature);
-double CalculFonction(double x);
-//void AffichageGnuplot(structEchantillonnage_fonction Echantillonnage_fonction);
-void SauvegardeCourbe(char * nom_fichier, double * Abscisses, double * Ordonnees, int n);
+}
+void verifSubdivision(structSubdivision Subdivision){
+		int i, n = Subdivision.nombre;
+		for(i=0; i<n; i++){
+				printf("\nintervalle #%d", i+1);
+				printf("\ndebut = %lf", Subdivision.tabIntervalle[i].debut);
+				printf("\nfin = %lf", Subdivision.tabIntervalle[i].fin);
+		}
+};
 
 #endif
--- integration/main.c
+++ integration/main.c
@@ -2,49 +2,112 @@
 #include <stdlib.h>
 #include "TP5_library.h"
 
-void show_array(double *array, int n){
-		int i;
-		for(i = 0; i < n; i++)
-        {
-				printf("\n Ligne[%d] = %lf",i, array[i]);
-		}	
 
-}
-void verifSubdivision(structSubdivision Subdivision){
-		int i, n = Subdivision.nombre;
-		for(i=0; i<n; i++){
-				printf("\nintervalle #%d", i+1);
-				printf("\ndebut = %lf", Subdivision.tabIntervalle[i].debut);
-				printf("\nfin = %lf", Subdivision.tabIntervalle[i].fin);
+// Fonction gÃ©nÃ©rique de sauvegarde de donnÃ©es pour le tracÃ© d'une courbe sur Gnuplot
+void SauvegardeCourbe(char * nom_fichier, double * Abscisses, double * Ordonnees, int n)
+{
+		FILE* fichier = NULL;
+		int k;
+
+		fichier = fopen(nom_fichier, "w");
+
+		if (fichier != NULL)
+		{
+		for(k=0;k<n;k++)
+		{
+			fprintf(fichier, "%f\t%f\n", Abscisses[k],Ordonnees[k]);
 		}
-};
+		fclose(fichier);
+		}
+}
+
+// Fonction crÃ©ant deux fichiers de donnÃ©es exploitables pour un tracÃ© sur Gnuplot
+void AffichageGnuplot(structEchantillonage_fonction Echantillonage_fonction)
+{
+			int n = Echantillonage_fonction.Subdivision_Echantillonage.nombre;
+			int p = Echantillonage_fonction.Quadrature_Echantillonage.taille;
+			int m = 1000;
+			int j,k;
+			double debut,fin,pointQuadrature,SommeZetaQuadrature;
+
+			double * Ordonnees_f_x;
+			double * Abscisses_x;
+			double * Ordonnees_fboxes_x;
+			double * Abscisses_boxes;
+			printf("\nCreating Memory");
+			Abscisses_x = (double*) malloc(m*sizeof(double));
+			printf(".");
+			Ordonnees_f_x = (double*) malloc(m*sizeof(double));
+			printf(".");
+			Abscisses_boxes = (double*) malloc((2*p*n+1)*sizeof(double));
+			printf(".");
+			Ordonnees_fboxes_x = (double*) malloc(2*p*n*sizeof(double));
+			printf(".");
+
+			Abscisses_boxes[0]=Echantillonage_fonction.Subdivision_Echantillonage.tabIntervalle[0].debut;
+			for(j=0;j<n;j++)
+			{
+				debut = Echantillonage_fonction.Subdivision_Echantillonage.tabIntervalle[j].debut;
+				fin = Echantillonage_fonction.Subdivision_Echantillonage.tabIntervalle[j].fin;
+				SommeZetaQuadrature=0;
+				for(k=0;k<p;k++)
+				{
+					pointQuadrature = Echantillonage_fonction.Quadrature_Echantillonage.tabZeta[k];
+					SommeZetaQuadrature += Echantillonage_fonction.Quadrature_Echantillonage.tabOmega[k];
+					//printf("%lf\n",SommeZetaQuadrature);
+					Ordonnees_fboxes_x[2*(p*j+k)] = Echantillonage_fonction.matriceFonctionEchantillonnee[k][j];
+					Ordonnees_fboxes_x[2*(p*j+k)+1] = Echantillonage_fonction.matriceFonctionEchantillonnee[k][j];
+					Abscisses_boxes[2*(p*j+k)+1] = (1-SommeZetaQuadrature) * debut + SommeZetaQuadrature * fin; 
+					Abscisses_boxes[2*(p*j+k)+2] = (1-SommeZetaQuadrature) * debut + SommeZetaQuadrature * fin; 
+				}
+			}
+
+			debut = Echantillonage_fonction.Subdivision_Echantillonage.tabIntervalle[0].debut;
+			fin = Echantillonage_fonction.Subdivision_Echantillonage.tabIntervalle[n-1].fin;
+			for(j=0;j<m;j++)
+			{
+				Abscisses_x[j] = debut * (m-j-0.5) / m + fin * (j+0.5) / m;
+				Ordonnees_f_x[j] = CalculFonction(Abscisses_x[j]);
+			}
+
+			SauvegardeCourbe("courbe_ligne.dat", Abscisses_x, Ordonnees_f_x, m);
+			SauvegardeCourbe("courbe_integration_boxes.dat", Abscisses_boxes, Ordonnees_fboxes_x, 2*p*n);
+
+			free(Ordonnees_fboxes_x);
+			free(Abscisses_x);
+			free(Ordonnees_f_x);
+			free(Abscisses_boxes);
+}
 
 int main(){
-		printf("\nVerification de Subdivision\n");
+		int n, type;
+		double I;
+
 		structSubdivision Subdivision;
 		structIntervalle Intervalle;
 
-		int i, n=60;
+		printf("\nSaisisez le debut de l'intervalle = ");
+		scanf("%lf", &Intervalle.debut);
+		printf("\nSaisisez la fin de l'intervalle = ");
+		scanf("%lf", &Intervalle.fin);
 
-		Intervalle.debut = 0;
-		Intervalle.fin =5;
-
+		printf("\nEcrivez le nombre intervalles = ");
+		scanf("%d", &n);
 		CalculerSubdivisionReguliere(n, Intervalle, &Subdivision);
 
-		verifSubdivision(Subdivision);
+		//verifSubdivision(Subdivision);
 
-    	//Ahora verificamos la quadrature
+    	//On lit le type de quadrature
+		printf("\nSaisisez le type de quadrature:\n1. Par trapezes\n2. Par Point Droit\n3. Par Point Gauche\n4. Par Point Millieu\n");
+		scanf("%d", &type);
+
 		structQuadrature Quadrature;
+        LireQuadrature(&Quadrature, type); 
 
-        LireQuadrature(&Quadrature); 
-
-        printf("\n\nX = ");
-		show_array(Quadrature.tabZeta, Quadrature.taille);
-		printf("\n\n");
-		printf("Y = ");
-		show_array(Quadrature.tabOmega, Quadrature.taille);
-           
 		structEchantillonage_fonction matriz = echantillonage_fonction(Subdivision, Quadrature);
+		I = CalculIntegrale(matriz);
+		//AffichageGnuplot(matriz);
+
 
 		return 0;
 }
